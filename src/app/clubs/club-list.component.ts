991f151d20f183c8eb80913c675607e6670968e6
Colin Roe <colinkroe@gmail.com>
2020-02-28 15:44:01 +0100

    Services and Dependency Injection
    I buildt a simple service, registered and injected the service into the club-list.component constructor

--- src/app/clubs/club-list.component.ts
+++ src/app/clubs/club-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IClubs } from "./clubs";
+import { ClubService } from './club-servcies';
 
 @Component({
   selector: "pm-clubs",
@@ -24,52 +25,9 @@ export class ClubListComponent implements OnInit {
   }
 
   filteredClubs: IClubs[];
-  clubs: IClubs[] = [
-    {
-      clubId: 1,
-      clubName: "Stuttgart GAA",
-      clubCode: "DE-12",
-      clubLocation: "Stuttgart",
-      clubImage: "assets/images/stuttgart_logo.png",
-      starRating: 3.5
-    },
-    {
-      clubId: 2,
-      clubName: "Darmstadt GAA",
-      clubCode: "DE-10",
-      clubLocation: "Darmstadt",
-      clubImage: "assets/images/darmstadt_logo.png",
-      starRating: 4.2
-    },
-    {
-      clubId: 3,
-      clubName: "Berlin GAA",
-      clubCode: "DE-8",
-      clubLocation: "Berlin",
-      clubImage: "assets/images/berlin_gaa_logo.png",
-      starRating: 4.0
-    },
-    {
-      clubId: 4,
-      clubName: "Zürich GAA",
-      clubCode: "DE-14",
-      clubLocation: "Zürich",
-      clubImage: "assets/images/zürich_gaa_logo.png",
-      starRating: 3.4
-    },
-    {
-      clubId: 5,
-      clubName: "Amsterdam GAA",
-      clubCode: "NL-6",
-      clubLocation: "Amsterdam",
-      clubImage: "assets/images/amsterdam_gaa_logo.png",
-      starRating: 4.6
-    }
-  ];
+  clubs: IClubs[] = [];
 
-  constructor() {
-    this.filteredClubs = this.clubs;
-    this.listFilter = "";
+  constructor(private clubService: ClubService) {    
   }
 
   onRatingClicked(message: string): void {
@@ -89,6 +47,7 @@ export class ClubListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log("In Oninit");
+    this.clubs = this.clubService.getClubs();
+    this.filteredClubs = this.clubs;
   }
 }
