a9f0e9451886b58a18194df9c10ee3b020693dd9
Colin Roe <colinkroe@gmail.com>
2020-03-24 12:05:57 +0100

    Integrated cross form validation for confirming emails
    Added a validator function to compare two email addresses and specified this validator for the nested form group, i.e. emailGroup

--- src/app/members/member.component.ts
+++ src/app/members/member.component.ts
@@ -1,32 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 
 import { Member } from "./member";
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get("email");
+  const confirmControl = c.get("confirmEmail");
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { match: true };
+}
+
 @Component({
-  selector: 'pm-member',
-  templateUrl: './member.component.html',
-  styleUrls: ['./member.component.css']
+  selector: "pm-member",
+  templateUrl: "./member.component.html",
+  styleUrls: ["./member.component.css"]
 })
 export class MemberComponent implements OnInit {
   memberForm: FormGroup;
   member = new Member();
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.memberForm = this.fb.group(
-      {
-        firstName: ['', [Validators.required, Validators.minLength(3)]],
-        lastName: ['', [Validators.required, Validators.maxLength(50)]],
-        email: ['', [Validators.required, Validators.email]],
-        addAddress: true
-      }
-    );
+    this.memberForm = this.fb.group({
+      firstName: ["", [Validators.required, Validators.minLength(3)]],
+      lastName: ["", [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ["", [Validators.required, Validators.email]],
+        confirmEmail: ["", Validators.required]
+      }, { validator: emailMatcher }),
+      
+      addAddress: true
+    });
   }
 
   save() {
     console.log(this.memberForm);
-    console.log('Saved: ' + JSON.stringify(this.memberForm.value));
+    console.log("Saved: " + JSON.stringify(this.memberForm.value));
   }
 }
